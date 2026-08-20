ce727a70c94ec079f0ef38f51e053ad80fa2db2f
Dhanusha <katakamedigadhanusha@gmail.com>
2026-08-20 18:55:57 +0530

    Update audio_utils.py

--- utils/audio_utils.py
+++ utils/audio_utils.py
@@ -1,12 +1,21 @@
 import ffmpeg
 import os
+
+
 def extract_audio(video_path):
-  audio_path=os.path.splitext(video_path)[0]+".wav"
-  (
-    ffmpeg
-    .input(video_path)
-    .output(audio_path)
-    .run(overwrite_output=True)
-  
-  )
-  return audio_path
+    # If the uploaded file is already an audio file,
+    # no audio extraction is required.
+    if video_path.lower().endswith((".wav", ".mp3", ".m4a", ".flac", ".aac", ".ogg")):
+        return video_path
+
+    # For video files, extract the audio as WAV.
+    audio_path = os.path.splitext(video_path)[0] + ".wav"
+
+    (
+        ffmpeg
+        .input(video_path)
+        .output(audio_path, ac=1, ar=16000)
+        .run(overwrite_output=True)
+    )
+
+    return audio_path
